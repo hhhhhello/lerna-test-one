54066c5baf1a0a7f3f055316b73c262192e2e2a3
zhengxiaomei <1123056007@qq.com>
2023-07-19 18:01:38 +0800

    lib

--- packages/print-two/index.js
+++ packages/print-two/index.js
@@ -1,5 +1,5 @@
 const print = () => {
-    console.log('hhh print 252234 222 feat: add new feature XYZ hhhh 对323 hhhhsss')
+    console.log('ahhh print 252234 222 feat: add new feature XYZ hhhh 对323 hhhhsss')
 }
 
 export default print
